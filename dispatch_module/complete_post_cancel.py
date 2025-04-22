01cd5d6d60b93ea4bbf1eb9add4280749a29d190
kiarieking <kiariekevin22@gmail.com>
2025-04-22 11:36:50 +0300

    [EDIT]: added web driver wait to email and password inputs.

--- dispatch_module/complete_post_cancel.py
+++ dispatch_module/complete_post_cancel.py
@@ -98,15 +98,6 @@ class UntitledTestCase(unittest.TestCase):
         status = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//button[@data-value='draft']")))
         title = status.get_attribute('title')
         assert title == "Current state"
-
-  
-
-        
-
-
-
-
-
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
